2821d9afc66be3bbaef4dab3b889f16d20d4268c
gsmoff <maslovsg@ukr.net>
2022-07-06 22:23:45 +0300

    add second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = "videoplayer-current-time";
+const saveData = localStorage.getItem(STORAGE_KEY);
+
+
+const onPlay = function (data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+    console.log('viewing time: ' + data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function playAgain() {
+    if (saveData !== null) {
+        player.setCurrentTime(saveData)
+    }
+};
+playAgain();
